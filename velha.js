797a5ca448be61db8dc477f581eb6b8b5593826f
antoniorony <antoniorony2013@gmail.com>
2022-04-29 14:14:56 -0300

    Add controles de checagem do vencedor

--- velha.js
+++ velha.js
@@ -4,17 +4,46 @@ class Velha {
         this._jogador = null;
         this.quadrado = null;
         this.jogadorSelecionado = document.getElementById('jogador-selecionado');
+        this.quadrados = document.getElementsByClassName('quadrado');
         this.mudarJogador('X');
     }
 
     escolherQuadrado(id) {
         this.quadrado = document.getElementById(id);
+        if (this.quadrado.innerHTML.trim() !== '-') {
+            return;
+        }
         this.quadrado.innerHTML = this._jogador;
         this.quadrado.style.color = '#000';
+
+        if (this._jogador === 'X') {
+            this._jogador = 'O';
+        } else {
+            this._jogador = 'X';
+        }
+
+        this.mudarJogador(this._jogador);
     }
 
     mudarJogador(valor) {
         this._jogador = valor;
         this.jogadorSelecionado.innerHTML = this._jogador;
+    }
+
+    checarVencedor() {
+        var quadrado1 = document.getElementById('1');
+        var quadrado1 = document.getElementById('2');
+        var quadrado1 = document.getElementById('3');
+        var quadrado1 = document.getElementById('4');
+        var quadrado1 = document.getElementById('5');
+        var quadrado1 = document.getElementById('6');
+        var quadrado1 = document.getElementById('7');
+        var quadrado1 = document.getElementById('8');
+        var quadrado1 = document.getElementById('9');
+        
+    }
+
+    checarSeguencia() {
+        
     }
 }
